e78e486626615b7ae6a05cbf81869d57b6dd2aa8
xodefender <kir.zhilnikov37@internet.ru>
2025-10-16 17:10:05 +0300

    add vlan and bridge display in nm-applet

--- src/applet.h
+++ src/applet.h
@@ -109,6 +109,8 @@ typedef struct {
 	NMADeviceClass *broadband_class;
 #endif
 	NMADeviceClass *bt_class;
+	NMADeviceClass *vlan_class;
+	NMADeviceClass *bridge_class;
 
 	/* Data model elements */
 	guint           update_icon_id;
